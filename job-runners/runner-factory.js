ff1406d93be7ab10892432317b50a95bf4651cb4
Grant Pidwell <grantpidwell@gmail.com>
2017-04-14 12:21:53 +0200

    creation of Docker and archive utilities; async in runner; dependency injection in runner constructor

--- job-runners/runner-factory.js
+++
@@ -1,8 +0,0 @@
-module.exports = RunnerFactory;
-
-function RunnerFactory() {
-}
-
-RunnerFactory.prototype.getRunnerModule = function (runnerType) {
-    throw new Error('getRunnerModule not yet implemented!');
-};
---
+++ test/unit/happn-protocol/runner-tests.js
@@ -0,0 +1,42 @@
+var expect = require('expect.js');
+var path = require('path');
+
+var Dockeriser = require("../../../lib/utils/dockeriser");
+var Runner = require("../../../lib/job-runners/happn-protocol/runner");
+
+var Mocker = require('mini-mock');
+
+describe('unit - happn-protocol job-runner', function () {
+
+    this.timeout(30000);
+
+    context('', function () {
+
+        beforeEach('setup', function (done) {
+
+            var mocker = new Mocker();
+
+            this.__mockDockeriser = mocker.mock(Dockeriser.prototype)
+                .withAsyncStub("buildImage")
+                .withAsyncStub("createContainer")
+                .withAsyncStub("runContainerAndReport")
+                .create();
+
+            done();
+        });
+
+        afterEach('stop', function (done) {
+            done();
+        });
+
+        context('start', function () {
+
+            it('successfully executes', function (done) {
+
+                var runner = new Runner();
+
+            });
+
+        });
+    });
+});
