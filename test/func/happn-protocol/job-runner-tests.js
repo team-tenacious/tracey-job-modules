e9c8cc321b3abdce655e057576f4c0db8422bd7a
leebow <grantpidwell@gmail.com>
2017-04-26 17:04:45 +0200

    Successfully committing and pushing to branch from runner

--- test/func/happn-protocol/job-runner-tests.js
+++ test/func/happn-protocol/job-runner-tests.js
@@ -1,5 +1,6 @@
 var expect = require('expect.js');
 var path = require('path');
+var fs = require('fs');
 
 var Giteriser = require('../../../lib/utils/giteriser');
 var Comparer = require('../../../lib/utils/comparer');
@@ -15,27 +16,33 @@ describe('unit - happn-protocol job-runner', function () {
         before('setup', function (done) {
 
             var self = this;
-            this.__giteriser = new Giteriser();
-            this.__comparer = new Comparer();
-            this.__commander = new Commander();
+            self.__comparer = new Comparer();
+            self.__commander = new Commander();
 
             this.__job = {
                 repo: "https://github.com/happner/happn-protocol.git",
                 folder: path.join(__dirname, '..', path.sep, '..', path.sep, 'tmp', path.sep, 'repos', path.sep, 'happn-protocol')
             };
 
-            // cleanup - CAREFUL!!!
-            this.__commander.run('rm -R ' + self.__job.folder, function (err, result) {
+            self.__commander.run('rm -R ' + self.__job.folder, function (err) {   // remove temp folder - CAREFUL!!!!!
 
-                if (err)
-                    console.log(err);
+                // make a new temp dir
+                fs.mkdirSync(self.__job.folder);
+
+                self.__giteriser = new Giteriser(self.__job.folder);
 
                 // clone the repo to temp
                 self.__giteriser.clone(self.__job.repo, self.__job.folder, function (err, result) {
                     if (err)
                         done(err);
 
-                    done();
+                    // switch to the test branch
+                    self.__giteriser.checkout('tracey-runner-updates', function (err, result) {
+                        if (err)
+                            done(err);
+
+                        done();
+                    });
                 });
             });
         });
@@ -50,17 +57,19 @@ describe('unit - happn-protocol job-runner', function () {
 
                 var self = this;
 
-                var runner = new Runner(self.__job, self.__commander, self.__comparer);
+                var runner = new Runner(self.__job, self.__commander, self.__giteriser);
 
                 runner.start(function (e, result) {
 
                     if (e)
                         return done(e);
 
-                    expect(result.length).not.to.be(0);
+                    //expect(result.length).not.to.be(0);
                     done();
                 });
             });
         });
-    });
-});
+    })
+    ;
+})
+;
