3771646de803217954f74a60928a5b444bc61ea8
leebow <grantpidwell@gmail.com>
2017-05-16 19:49:05 +0200

    Added new error raising test

--- test/unit/performance-tracker-lite/job-runner-tests.js
+++ test/unit/performance-tracker-lite/job-runner-tests.js
@@ -20,38 +20,37 @@ describe('unit - performance-tracker-lite job-runner', function () {
 
         beforeEach('setup', function (done) {
 
-            var mocker = new Mocker();
+            this.__mocker = new Mocker();
 
-            this.__mockTester = mocker.mock(Tester.prototype)
+            this.__mockTester = this.__mocker.mock(Tester.prototype)
                 .withSyncStub("test", new Promise(function (resolve, reject) {
-                    console.log('TESTING 123');
                     resolve({blah: 'blah'})
                 }))
                 .create();
 
-            this.__mockVersionUtil = mocker.mock(VersionUtil.prototype)
+            this.__mockVersionUtil = this.__mocker.mock(VersionUtil.prototype)
                 .withSyncStub("getVersions", ['6'])
                 .create();
 
-            this.__mockStateUpdater = mocker.mock(StateUpdater.prototype)
+            this.__mockStateUpdater = this.__mocker.mock(StateUpdater.prototype)
                 .withSyncStub("updateState")
                 .create();
 
-            this.__mockUploader = mocker.mock(Uploader.prototype)
+            this.__mockUploader = this.__mocker.mock(Uploader.prototype)
                 .withSyncStub("upload", new Promise(function (resolve, reject) {
                     resolve({});
                 }))
                 .create();
 
-            this.__mockLogUpdater = mocker.mock(LogUpdater.prototype)
+            this.__mockLogUpdater = this.__mocker.mock(LogUpdater.prototype)
                 .withSyncStub("done")
                 .create();
 
-            this.__mockBlackBoard = mocker.mock(BlackBoard.prototype)
+            this.__mockBlackBoard = this.__mocker.mock(BlackBoard.prototype)
                 .withSyncStub("chalkRed")
                 .create();
 
-            this.__mockFiler = mocker.mock(FileResolver.prototype)
+            this.__mockFiler = this.__mocker.mock(FileResolver.prototype)
                 .withSyncStub("getPackageJSON", {
                     name: 'test-package', version: '1.0.0',
                     description: 'Test package.json', main: 'index.js'
@@ -98,10 +97,10 @@ describe('unit - performance-tracker-lite job-runner', function () {
                     this.__mockFiler);
 
                 runner.start(function (e, result) {
-
                     if (e)
                         return done(e);
 
+                    // assertions
                     expect(self.__mockTester.recorder['test'].calls).to.equal(1);
                     expect(self.__mockVersionUtil.recorder['getVersions'].calls).to.equal(1);
                     expect(self.__mockStateUpdater.recorder['updateState'].calls).to.equal(3);
@@ -109,8 +108,61 @@ describe('unit - performance-tracker-lite job-runner', function () {
                     expect(self.__mockLogUpdater.recorder['done'].calls).to.equal(1);
                     expect(self.__mockBlackBoard.recorder['chalkRed'].calls).to.equal(0);
                     expect(self.__mockFiler.recorder['getPackageJSON'].calls).to.equal(1);
+                });
 
+                runner.on('run-complete', function () {
+                    // call done only when we receive the run-complete event
                     done();
+                });
+
+            });
+
+            it('raises an error', function (done) {
+
+                var self = this;
+
+                this.__mockTester = this.__mocker.mock(Tester.prototype)
+                    .withSyncStub("test", new Promise(function (resolve, reject) {
+                        reject({blah: 'blah'})
+                    }))
+                    //.withSyncStub("test", new Error({blah:'blah'}))
+                    .create();
+
+                var mockJob = {
+                    folder: path.sep + "blah",
+                    message: {
+                        config: {},
+                        job_type: {
+                            settings: {}
+                        },
+                        event: {
+                            owner: 'Bob'
+                        }
+                    },
+                    config: {
+                        testFolder: '/'
+                    }
+                };
+
+                var runner = new Runner(mockJob,
+                    this.__mockTester,
+                    this.__mockVersionUtil,
+                    this.__mockStateUpdater,
+                    this.__mockUploader,
+                    this.__mockLogUpdater,
+                    this.__mockBlackBoard,
+                    this.__mockFiler);
+
+                runner.start(function (e, result) {
+
+                    // assertions
+                    expect(self.__mockTester.recorder['test'].calls).to.equal(1);
+                    expect(self.__mockVersionUtil.recorder['getVersions'].calls).to.equal(1);
+
+                    if (e)
+                        return done();
+
+                    done(new Error('Error was expected!'));
                 });
 
             });
