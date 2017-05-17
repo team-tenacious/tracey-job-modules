bd6fe3555536388b85c0a54c46ee40f1d6814cd6
leebow <grantpidwell@gmail.com>
2017-05-17 09:59:26 +0200

    Resolving further issues with promise rejections

--- test/unit/performance-tracker-lite/job-runner-tests.js
+++ test/unit/performance-tracker-lite/job-runner-tests.js
@@ -65,108 +65,137 @@ describe('unit - performance-tracker-lite job-runner', function () {
             done();
         });
 
-        context('start', function () {
 
-            it('successfully executes', function (done) {
+        it('successfully executes', function (done) {
 
-                var self = this;
+            var self = this;
+            var mockJob = createMockJob();
 
-                var mockJob = {
-                    folder: path.sep + "blah",
-                    message: {
-                        config: {},
-                        job_type: {
-                            settings: {}
-                        },
-                        event: {
-                            owner: 'Bob'
-                        }
-                    },
-                    config: {
-                        testFolder: '/'
-                    }
-                };
+            var runner = new Runner(mockJob,
+                this.__mockTester,
+                this.__mockVersionUtil,
+                this.__mockStateUpdater,
+                this.__mockUploader,
+                this.__mockLogUpdater,
+                this.__mockBlackBoard,
+                this.__mockFiler);
 
-                var runner = new Runner(mockJob,
-                    this.__mockTester,
-                    this.__mockVersionUtil,
-                    this.__mockStateUpdater,
-                    this.__mockUploader,
-                    this.__mockLogUpdater,
-                    this.__mockBlackBoard,
-                    this.__mockFiler);
+            runner.start(function (e, result) {
+                if (e)
+                    return done(e);
 
-                runner.start(function (e, result) {
-                    if (e)
-                        return done(e);
-
-                    // assertions
-                    expect(self.__mockTester.recorder['test'].calls).to.equal(1);
-                    expect(self.__mockVersionUtil.recorder['getVersions'].calls).to.equal(1);
-                    expect(self.__mockStateUpdater.recorder['updateState'].calls).to.equal(3);
-                    expect(self.__mockUploader.recorder['upload'].calls).to.equal(1);
-                    expect(self.__mockLogUpdater.recorder['done'].calls).to.equal(1);
-                    expect(self.__mockBlackBoard.recorder['chalkRed'].calls).to.equal(0);
-                    expect(self.__mockFiler.recorder['getPackageJSON'].calls).to.equal(1);
-                });
-
-                runner.on('run-complete', function () {
-                    // call done only when we receive the run-complete event
-                    done();
-                });
-
+                // assertions
+                expect(self.__mockTester.recorder['test'].calls).to.equal(1);
+                expect(self.__mockVersionUtil.recorder['getVersions'].calls).to.equal(1);
+                expect(self.__mockStateUpdater.recorder['updateState'].calls).to.equal(3);
+                expect(self.__mockUploader.recorder['upload'].calls).to.equal(1);
+                expect(self.__mockLogUpdater.recorder['done'].calls).to.equal(1);
+                expect(self.__mockBlackBoard.recorder['chalkRed'].calls).to.equal(0);
+                expect(self.__mockFiler.recorder['getPackageJSON'].calls).to.equal(1);
             });
 
-            it('raises an error', function (done) {
-
-                var self = this;
-
-                this.__mockTester = this.__mocker.mock(Tester.prototype)
-                    .withSyncStub("test", new Promise(function (resolve, reject) {
-                        reject({blah: 'blah'})
-                    }))
-                    //.withSyncStub("test", new Error({blah:'blah'}))
-                    .create();
-
-                var mockJob = {
-                    folder: path.sep + "blah",
-                    message: {
-                        config: {},
-                        job_type: {
-                            settings: {}
-                        },
-                        event: {
-                            owner: 'Bob'
-                        }
-                    },
-                    config: {
-                        testFolder: '/'
-                    }
-                };
-
-                var runner = new Runner(mockJob,
-                    this.__mockTester,
-                    this.__mockVersionUtil,
-                    this.__mockStateUpdater,
-                    this.__mockUploader,
-                    this.__mockLogUpdater,
-                    this.__mockBlackBoard,
-                    this.__mockFiler);
-
-                runner.start(function (e, result) {
-
-                    // assertions
-                    expect(self.__mockTester.recorder['test'].calls).to.equal(1);
-                    expect(self.__mockVersionUtil.recorder['getVersions'].calls).to.equal(1);
-
-                    if (e)
-                        return done();
-
-                    done(new Error('Error was expected!'));
-                });
-
+            runner.on('run-complete', function () {
+                // call done only when we receive the run-complete event
+                done();
             });
 
         });
+
+        it('raises an error from the Tester', function (done) {
+
+            var self = this;
+
+            var testerError = {error: 'Tester error!'};
+            var expectedError = {'6': testerError}; // node 6, with errors
+
+            this.__mockTester = this.__mocker.mock(Tester.prototype)
+                .withSyncStub("test", new Promise(function (resolve, reject) {
+                    reject(testerError)
+                }))
+                .create();
+
+            var mockJob = createMockJob();
+
+            var runner = new Runner(mockJob,
+                this.__mockTester,
+                this.__mockVersionUtil,
+                this.__mockStateUpdater,
+                this.__mockUploader,
+                this.__mockLogUpdater,
+                this.__mockBlackBoard,
+                this.__mockFiler);
+
+            runner.start(function (e, result) {
+
+                // assertions
+                expect(self.__mockTester.recorder['test'].calls).to.equal(1);
+                expect(self.__mockVersionUtil.recorder['getVersions'].calls).to.equal(1);
+
+                if (e) {
+                    expect(e).to.eql(expectedError);
+                    return done();
+                }
+
+                done(new Error('Error was expected!'));
+            });
+
+        });
+
+        it('raises an error from the Uploader', function (done) {
+
+            var self = this;
+
+            var uploadError = {error: 'Upload rejected!'};
+            var expectedError = {'6': uploadError}; // node 6, with errors
+
+            this.__mockUploader = this.__mocker.mock(Uploader.prototype)
+                .withSyncStub("upload", new Promise(function (resolve, reject) {
+                    reject(uploadError);
+                }))
+                .create();
+
+            var mockJob = createMockJob();
+
+            var runner = new Runner(mockJob,
+                this.__mockTester,
+                this.__mockVersionUtil,
+                this.__mockStateUpdater,
+                this.__mockUploader,
+                this.__mockLogUpdater,
+                this.__mockBlackBoard,
+                this.__mockFiler);
+
+            runner.start(function (e, result) {
+
+                if (e) {
+                    expect(e).to.eql(expectedError);
+                    return done();
+                }
+
+                done(new Error('Error was expected!'));
+            });
+
+        });
+
     });
+
+
+    function createMockJob() {
+
+        return {
+            folder: path.sep + "blah",
+            message: {
+                config: {},
+                job_type: {
+                    settings: {}
+                },
+                event: {
+                    owner: 'Bob'
+                }
+            },
+            config: {
+                testFolder: '/'
+            }
+        };
+    };
 });
