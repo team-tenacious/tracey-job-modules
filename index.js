4a2fc751611325bdc7a53e3938381543d0065c9f
leebow <grantpidwell@gmail.com>
2017-07-25 09:47:17 +0200

    Modifed index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
     HappnProtocolRunner: require('./lib/job-runners/happn-protocol/runner'),
+    HappnerProtocolRunner: require('./lib/job-runners/happner-protocol/runner'),
     PerformanceTrackerRunner: require('./lib/job-runners/performance-tracker-lite/runner')
 };
