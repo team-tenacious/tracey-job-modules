d597e4e4a317d9539754f7224a0026f65220862a
leebow <grantpidwell@gmail.com>
2017-08-07 17:41:54 +0200

    Modified index.js

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ module.exports = {
     HappnerProtocolRunner: require('./lib/job-runners/happner-protocol/runner'),
     PerformanceTrackerRunner: require('./lib/job-runners/performance-tracker-lite/runner')
 };
+
+var exports = module.exports;
+
+// export the utils
+require('fs').readdirSync(__dirname + '/lib/utils').forEach(function (file) {
+    var name = formatName(file.replace('.js', ''));
+    exports[name] = require(__dirname + '/lib/utils/' + file);
+});
+
+function formatName(s) {
+
+    var capitalise = function (str) {
+        return str && str[0].toUpperCase() + str.slice(1);
+    };
+
+    if (s.indexOf('-') > -1) {
+        var strArr = s.split('-');
+        var result = '';
+
+        strArr.forEach(function (x) {
+            result += capitalise(x);
+        });
+
+        return result;
+    } else
+        return capitalise(s);
+}
